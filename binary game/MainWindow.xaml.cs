d0e20f4112354df56193b5a6b09620d7d22622b9
Olive568 <luislabapis@gmail.com>
2024-03-05 22:44:20 +0800

    Added theme music

--- binary game/MainWindow.xaml.cs
+++ binary game/MainWindow.xaml.cs
@@ -13,22 +13,25 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Media;
 
 namespace binary_game
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-
+        private SoundPlayer player;
         public MainWindow()
         {
             InitializeComponent();
+            player = new SoundPlayer(@"C:\Users\Luis Oliver\source\repos\binary-game\binary game\bin\Debug\Theme.wav");
+            player.Play();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            player.Stop();
             Window1 w1 = new Window1();
             w1.Show();
             this.Close();
@@ -37,6 +40,7 @@ namespace binary_game
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            player.Stop();
             Leaderboards ld = new Leaderboards();
             ld.Show();
             this.Close();
