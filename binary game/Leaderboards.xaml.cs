a91ab0129f8548bf5422bce7a816884566b867ca
Olive568 <luislabapis@gmail.com>
2024-03-17 23:21:42 +0800

    Leaderboards done

--- binary game/Leaderboards.xaml.cs
+++ binary game/Leaderboards.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.IO;
 using System.Windows.Media;
 
+
 namespace binary_game
 {
     public partial class Leaderboards : Window
@@ -14,32 +15,18 @@ namespace binary_game
         public Leaderboards()
         {
             InitializeComponent();
-            
 
             // Read data from the database
             Database DB = new Database();
             db = DB.Read_File();
 
-            // Add data to the ListBox
-            LstBox.Items.Add($"{db[0][0]}         {db[0][1]}              {db[0][2]}");
-            for (int x = 1; x < db.Count; x++)
+            // Add data to the ListView
+            foreach (string[] entry in db)
             {
-                string person = "";
-                for (int y = 0; y < db[x].Length; y++)
-                {
-                    if (y == 0 && db[x][y].Length > 6)
-                    {
-                        person += db[x][y] + "\t";
-                    }
-                    else
-                    {
-                        person += db[x][y] + "\t" + "    ";
-                    }
-                }
-                LstBox.Items.Add($"{person}");
+                ListViewItem item = new ListViewItem();
+                item.Content = new { Name = entry[0], Score = entry[1], Time = entry[2] };
+                ListViewLeaderboard.Items.Add(item);
             }
-
-     
         }
     }
 }
